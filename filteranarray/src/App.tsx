7498e3c73e429492233c05d41a42fc851ba81178
meshach567 <arinzemeshach123@gmail.com>
2024-09-11 10:32:18 +0100

    commit

---
+++ filteranarray/src/App.tsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+
+const fruits = [
+  "Apple",
+  "Pawpaw",
+  "PineAapple",
+  "Cucumber",
+  "Orange",
+  "Banana",
+  "Mango",
+  "Almond",
+  "pear",
+  "Watermelon"
+]
+
+function App() {
+  const [fruitsData] = useState(fruits);
+  const [searchTerm, setSearchTerms] = useState('');
+
+  const handleChange = (e: any) => {
+    setSearchTerms(e.target.value);
+    
+  }
+
+  const fruitsDataFiltered = fruitsData.filter((fruit) => fruit.toLocaleLowerCase().includes(searchTerm .toLocaleLowerCase())
+  )
+
+  return (
+    <div>
+     <input onChange={handleChange} type={'text'} placeholder='search'  />
+     {fruitsDataFiltered.map((fruit, index) => {
+      return <div key={index}>
+        <p>{fruit}</p>
+      </div>;
+     })}
+
+    </div>
+  )
+}
+
+export default App
